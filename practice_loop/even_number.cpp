a6689ccb824ab8655c3f53ce59beabcade38a9c6
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-10-11 17:26:30 +0530

    remark

---
+++ practice_loop/even_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int number = 1; number <= 10; number++)
+    {
+        if (number % 2 == 0)
+        {
+            cout << "even number = " << number << "\n";
+        }
+        
+    }
+    return 0;
+    
+}
