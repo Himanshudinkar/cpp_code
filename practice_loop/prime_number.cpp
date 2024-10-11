f3e0a96ce3c2b934e1e6e0bfa4c243747eca8c69
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-10-11 19:27:09 +0530

    remark

---
+++ practice_loop/prime_number.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "enter number to chake prime number" << "\n";
+    int number = 0;
+    cin >> number;
+
+    int i = number - 1;
+
+    bool f;
+
+    do
+    {
+        if (number % i == 0)
+        {
+            f = true;
+            break;
+        }
+        i--;
+        
+    } while (i > 1);
+
+    if (f == false)
+    {
+        cout << "prime number" << "\n";
+    }
+    else
+    {
+        cout << "not prime number" << "\n";
+    }
+
+    return 0;
+    
+    
+}
