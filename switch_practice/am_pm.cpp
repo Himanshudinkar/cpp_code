9737900f062e89422f664fe9d2c2fd7985035d25
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-10-15 01:30:36 +0530

    remark

--- switch_practice/am_pm.cpp
+++ switch_practice/am_pm.cpp
@@ -2,39 +2,11 @@
 using namespace std;
 int main()
 {
-    cout << "enter hours(0-23)" << "\n";
-    int h = 0;
-    cin >> h;
+    cout << "enter fahrenheit" << "\n";
+    float f = 0;
+    cin >> f;
 
-    cout << "enter minute(0-59)" << "\n";
-    int m = 0;
-    cin >> m;
+    float c = (f - 32) * 5.0/9;
 
-    switch (h/12)
-    {
-        case 0:
-        {
-            cout << "hours" << h << ":" << m << "AM" << "\n";
-            break;
-        }
-        case 1:
-        {
-            int k =(h-12) == 0?0:h;
-            if (k == 0)
-            {
-                cout << "00:00" << "AM" << "\n";
-            }
-            else
-            {
-                cout << k-12 << ":" << "PM" << "\n";
-            }
-            break;
-        }
-        default:
-        {
-            cout << "invalid value";
-            break;
-        }
-    }
-    return 0;
+    cout << c;
 }
