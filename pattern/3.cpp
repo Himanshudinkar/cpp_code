54e0c7491b19f9f3d28720ce185be3239cac2f7c
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-10-21 00:22:51 +0530

    remark

---
+++ pattern/3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r = 3;
+    int num = 1;
+
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= i ; j++)
+        {
+            cout << num;
+            num++;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
