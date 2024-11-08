3984c648209191d8d2be63fd0ebb01a8815b1d8d
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-11-08 22:52:18 +0530

    remark

--- Array/array.cpp
+++ Array/array.cpp
@@ -3,12 +3,10 @@ using namespace std;
 int main()
 {
     int arr[] = {10,23,43,12,34};
-    // for (int i = 0; i < 5; i++)
-    // {
-    //     cout << arr[i] << "\t";
-    // }
-
-    cout << arr[3];
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << "\t";
+    }
     
     return 0;
 } 
