494bda31545c16244ba839102f9a2c59d7e7ad1d
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-11-11 22:30:09 +0530

    remark

---
+++ Array/min_value.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr [] = {19,8,4,5,6};
+    int s = 0;
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i = 0; i < len; i++)
+    {
+        s = s + arr[i]; 
+    }
+    cout << s << "\t";
+    return 0;
+}
