8b5dd0281862cd05d8a444d9276bf7b9dbb07e37
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-11-13 23:35:59 +0530

    remark

--- Array/prime_number.cpp
+++ Array/prime_number.cpp
@@ -1,5 +1,4 @@
 //wap to display prime number given array input {7,4,6,11,5} output = 7,11,5
-//2 wap to replace 1 with 0 in a givn array {2,1,3,1,5,1} output {2,0,3,0,5,0}
 
 #include <iostream>
 using namespace std;
