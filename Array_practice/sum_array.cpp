52e40d0b69f2f6c6f7d8fc187c50271a472f45e3
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-11-17 12:59:41 +0530

    sum of array

---
+++ Array_practice/sum_array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {34,54,2,45,3,6,9,1,};
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    int s = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        s = s + arr[i];
+    }
+    cout << s << "\n";
+
+    return 0;
+}
