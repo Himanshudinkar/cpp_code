64d21ec623e0b06cd64d212e1efebc54d2ea8481
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-11-18 23:12:12 +0530

    user input 2d array

---
+++ array_2d/user_input.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[2][3];
+    cout << "enter 6 value" << "\n";
+    for (int r = 0; r < 2; r++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            
+            cin >> arr[r][c];
+        }
+        
+    }
+    cout << "\n" << "after input" << "\n";
+    
+     for (int r = 0; r < 2; r++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            cout << arr[r][c] << "\t";
+        }
+        cout << "\n"; 
+    }
+   return 0;
+}
