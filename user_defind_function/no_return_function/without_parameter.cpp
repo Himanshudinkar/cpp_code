2286148e973204361c3426555dc8f5162f989e46
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-11-29 00:11:21 +0530

    function

---
+++ user_defind_function/no_return_function/without_parameter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//no return function me ye without parameter he matlab isme kuchh calculation nahi kar sakte he bas 
+//print kara sakte he bana banaya
+//no retun function void data type se decalre karte he
+//void ka matlab hota he jise bas dekha ja sakta he change nahi kar sakte he
+void run(){
+    cout << "hello" << "\n";
+}
+
+int main()
+{
+run();
+run();
+
+return 0;
+}
