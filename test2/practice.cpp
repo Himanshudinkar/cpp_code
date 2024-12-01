bd3f0725758fc65cf2ea243b94f898ba6b9a1ebb
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-12-02 00:02:29 +0530

    practice

--- test2/practice.cpp
+++ test2/practice.cpp
@@ -1,41 +1,77 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[] = {7,5,6,8,11,92,18,19};
+//     int len = sizeof(arr)/sizeof(arr[0]);
+
+//     cout << "array like" << "\n";
+
+//     for (int i = 0; i < len; i++)
+//     {
+//         cout << arr[i] << "\t";
+//     }
+
+//     cout << "\n prime number \n";
+
+//     for (int j = 0; j < len; j++)
+//     {
+//         int p = arr[j] - 1;
+//         bool f = false;
+
+//         do
+//         {
+//             if (arr[j]%p == 0)
+//             {
+//                 f = true;
+//             }
+//             p--;
+            
+//         } while (p > 1);
+
+//         if (f == false)
+//         {
+//             cout << arr[j] << "\t";
+//         }
+        
+       
+//     }
+//      return 0;
+    
+// }
+
+
 #include <iostream>
 using namespace std;
 int main()
 {
-    int arr[] = {7,5,6,8,11,92,18,19};
+    int arr[] = {64,24,81,312,25,121,76,625};
     int len = sizeof(arr)/sizeof(arr[0]);
 
-    cout << "array like" << "\n";
-
+    cout << "given array" << "\n";
+    
     for (int i = 0; i < len; i++)
     {
         cout << arr[i] << "\t";
     }
+    
 
-    cout << "\n prime number \n";
+    cout << "\n valid square of this number\n";
 
     for (int j = 0; j < len; j++)
     {
-        int p = arr[j] - 1;
-        bool f = false;
+        int s = arr[j];
 
-        do
+        for (int x = 1; x <= s; x++)
         {
-            if (arr[j]%p == 0)
+            if (x*x == s)
             {
-                f = true;
+                cout << s << "\t";
+                break;
             }
-            p--;
-            
-        } while (p > 1);
-
-        if (f == false)
-        {
-            cout << arr[j] << "\t";
+           
         }
         
-       
     }
-     return 0;
-    
+    return 0;
 }
