8365d7de2157d87454f07e1067f10561fc6da324
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-12-25 22:55:46 +0530

    class

--- OPPs/class/factorial_composite.cpp
+++ OPPs/class/factorial_composite.cpp
@@ -2,3 +2,32 @@
 
 #include <iostream>
 using namespace std;
+
+class virus
+{
+    public: void affect()
+    {
+        cout << "ur hecked" << "\n";
+    }
+};
+
+class reward
+{
+    virus v;
+    public:void prize()
+    {
+        cout << "u won 200" << "\n";
+
+         v.affect();
+    }
+
+   
+};
+
+int main()
+{
+    reward r;
+    r.prize();
+
+    return 0;
+}
